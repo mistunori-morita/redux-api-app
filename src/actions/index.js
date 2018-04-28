dd230e100c7bd671949069bee54e2ffbd8186dce
mitsunori-morita <mitsunori.morita@speee.jp>
2018-04-28 12:05:12 +0900

    createactinons setting

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,11 @@ const startGetUsers = () => { return  { type: 'START_GET_USERS', ready: false }}
 const completeGetUsers = (data) => { return { type: 'COMPLETE_GET_USERS', data }}
 const errorGetUsers = (err) => { return { type: 'ERROR_GET_USERS', err }}
 
+
+const startGetUserById = () => { return  { type: 'START_GET_USER_BY_ID', ready: false }}
+const completeGetUserById = (data) => { return { type: 'COMPLETE_GET_USER_BY_ID', data }}
+const errorGetUserById = (err) => { return { type: 'ERROR_GET_USER_BY_ID', err }}
+
 export const getUsers = () => {
 
   return (dispatch, getState) =>{
@@ -17,6 +22,23 @@ export const getUsers = () => {
       })
       .catch((err) => {
         dispatch(errorGetUsers(err));
+      })
+   
+  }
+}
+
+export const getUserById = (userId) => {
+
+  return (dispatch, getState) =>{
+    dispatch(startGetUserById());
+    http.get('users/' + userId)
+      .then((response) =>{
+        if(response.data)
+        dispatch(completeGetUserById(response.data));
+        console.log(response)
+      })
+      .catch((err) => {
+        dispatch(errorGetUserById(err));
       })
    
   }
--- src/views/UserDetail.js
+++ src/views/UserDetail.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 
 
 class UserDetail extends Component{
+  componentWillMount(){
+    console.log(this.props.match.params.userId)
+  }
   render(){
+    console.log(this.props);
     return(
       <div>
         <h2>Haloa</h2>
